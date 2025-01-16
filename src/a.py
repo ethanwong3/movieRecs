77305c0132b6d54b1c16720416a96d4e78333711
Ethan <ethaneswanebban@gmail.com>
2025-01-16 13:35:26 +1100

    preparing for propject finish, deleted unecessary debuggin script

--- src/a.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-genre_similarity = np.load("data/genre_similarity_matrix.npy")
-tag_similarity = np.load("data/tag_similarity_matrix.npy")
-ratings_similarity = np.load("data/ratings_similarity_matrix.npy")
-
-print("Genre Similarity Matrix Sample:", genre_similarity[0][:10])
-print("Tag Similarity Matrix Sample:", tag_similarity[0][:10])
-print("Ratings Similarity Matrix Sample:", ratings_similarity[0][:10])
